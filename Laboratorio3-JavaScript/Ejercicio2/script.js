682a46e16529c41cf0cc9e6842f6f35298427370
Diego <dsantacruz@unsa.edu.pe>
2025-04-30 08:24:11 -0500

    Creando rama para las tareas de teoria y directoria para la tarea AJAX

--- Laboratorio3-JavaScript/Ejercicio2/script.js
+++
@@ -1,5 +0,0 @@
-function invertirTexto() {
-    let textoEntrada = document.getElementById("textoEntrada").value;
-    let textoAlReves = textoEntrada.split("").reverse().join("");
-    document.getElementById("resultado").textContent = textoAlReves;
-  } 
