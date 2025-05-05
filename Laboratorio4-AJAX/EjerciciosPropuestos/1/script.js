2a1c3c598d7d0d75216698f8afe7014b90e40663
Diego <dsantacruz@unsa.edu.pe>
2025-05-05 17:45:33 -0500

    Creando el html con un botom que muestre la tabla(lista) de regiones, creando las funciones del javascript que conecte la data y cree la tabla

---
+++ Laboratorio4-AJAX/EjerciciosPropuestos/1/script.js
@@ -0,0 +1,38 @@
+
+google.charts.load('current', { packages: ['table'] });
+let datos = [];
+
+function mostrarRegiones(){
+    google.charts.setOnLoadCallback(cargarRegiones);
+}
+
+function cargarRegiones() {
+    fetch('../data.json')
+      .then(function(response) {
+        return response.json(); // Convertir la respuesta en JSON
+      })
+      .then(function(data) {
+        datos = data; 
+
+        let listaRegiones = datos.map(function(item) {
+          return item.region;
+        });
+
+        mostrarTablaRegiones(listaRegiones);
+      })
+      .catch(function(error) {
+        console.error('Error al cargar JSON:', error);
+      });
+  }
+
+  function mostrarTablaRegiones(regiones) {
+    let dataTable = new google.visualization.DataTable();
+    dataTable.addColumn('string', 'Región');
+  
+    regiones.forEach(function(a) {
+      dataTable.addRow([a]);
+    });
+  
+    let table = new google.visualization.Table(document.getElementById('tablaRegiones'));
+    table.draw(dataTable, { showRowNumber: true, width: '400px', height: 'auto' });
+  }
