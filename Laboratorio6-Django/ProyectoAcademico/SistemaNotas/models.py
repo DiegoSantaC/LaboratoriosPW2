9059564c051442d8b16cdd6099532e5ba58a67fa
Diego <dsantacruz@unsa.edu.pe>
2025-05-24 11:39:11 -0500

    Cambiendo el idioma a español y creando la clase Estudiante, curso, primer migrate

--- Laboratorio6-Django/ProyectoAcademico/SistemaNotas/models.py
+++ Laboratorio6-Django/ProyectoAcademico/SistemaNotas/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Alumno(models.Model):
+    CUI = models.CharField(max_length=100, unique=True)
+    nombres = models.CharField(max_length=200)
+    apellidos = models.CharField(max_length=200)
+ 
+    def __str__(self):
+    	return f"Alumno:  {self.nombres } {self.apellidos} {self.CUI}"
+ 
+class Curso(models.Model):
+    codigo = models.CharField(max_length=100, unique=True)
+    nombre = models.CharField(max_length=200)
+ 
+    def __str__(self):
+        return f"Curso:  {self.nombre} codigo: {self.codigo}"
