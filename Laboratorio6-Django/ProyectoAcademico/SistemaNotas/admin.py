97d4c3b997a08265528e9d5cb218dfc3dfcdb7ef
Diego <dsantacruz@unsa.edu.pe>
2025-05-24 12:24:07 -0500

    Creando la clase(tabla) NotasAlumnosPorCurso e importandola en admin para ser visualizada, migrate 3

--- Laboratorio6-Django/ProyectoAcademico/SistemaNotas/admin.py
+++ Laboratorio6-Django/ProyectoAcademico/SistemaNotas/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Alumno,Curso
+from .models import Alumno,Curso,NotasAlumnosPorCurso
 
 # Register your models here.
 admin.site.register(Alumno)
 admin.site.register(Curso)
+admin.site.register(NotasAlumnosPorCurso)
--- Laboratorio6-Django/ProyectoAcademico/SistemaNotas/models.py
+++ Laboratorio6-Django/ProyectoAcademico/SistemaNotas/models.py
@@ -1,17 +1,27 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Alumno(models.Model):
-    CUI = models.CharField(max_length=100, unique=True)
+    CUI = models.CharField(max_length=100, unique=True, primary_key=True)
     nombres = models.CharField(max_length=200)
     apellidos = models.CharField(max_length=200)
  
     def __str__(self):
-    	return f"{self.nombres } {self.apellidos} {self.CUI}"
+    	return f"{self.nombres } {self.apellidos} , CUI: {self.CUI}"
  
 class Curso(models.Model):
-    codigo = models.CharField(max_length=100, unique=True)
+    codigo = models.CharField(max_length=100, unique=True, primary_key=True)
     nombre = models.CharField(max_length=200)
  
     def __str__(self):
-        return f"Curso:  {self.nombre} codigo: {self.codigo}"
+        return f"{self.nombre} , codigo: {self.codigo}"
+    
+class NotasAlumnosPorCurso(models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nota = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(20)])   
+
+ 
+    def __str__(self):
+        return f"Nota: {self.nota}  Alumno: {self.alumno.CUI} , Curso: {self.curso.codigo}"
