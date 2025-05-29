dbcc84db59769c33be7b9dcc9eaad1f86f1ce1ac
Diego <dsantacruz@unsa.edu.pe>
2025-05-28 19:18:51 -0500

    Modificando el modela personas:cambiando el estilo de los campos y agregando un toString

--- tareaDjango/listaContactos/personas/models.py
+++ tareaDjango/listaContactos/personas/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
-    nombres = models.TextField()
-    apellidos = models.TextField()
-    edad = models.TextField()
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    edad = models.IntegerField()
     donador= models.BooleanField()
+
+    def __str__(self):
+        return f"{self.nombres} {self.apellidos} ,edad:{self.edad} ,es donador:{self.donador}"
