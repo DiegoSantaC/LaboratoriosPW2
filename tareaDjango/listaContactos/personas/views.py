c704cffc1c356f06ab8b0497f9b7a425712fc483
Diego <dsantacruz@unsa.edu.pe>
2025-05-29 15:56:18 -0500

    Usando los datos de una persona como contexto y enviandolos a un template para mostrarlos en pantalla, usando estos datos para el if(verficar su estado de salud)

--- tareaDjango/listaContactos/personas/views.py
+++ tareaDjango/listaContactos/personas/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Persona
 
 # Create your views here.
+def personaTestView(request):
+    obj = Persona.objects.get(id = 3)
+    myContext = {
+        'nombres': obj.nombres,
+        'apellidos': obj.apellidos,
+        'edad' : obj.edad,
+    }
+    return render(request,"personas/test.html",myContext)
