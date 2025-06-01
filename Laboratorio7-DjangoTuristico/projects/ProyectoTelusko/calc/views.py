887e8e1a7b8fd562cee15a18ae21561c44ae1cf7
Diego <dsantacruz@unsa.edu.pe>
2025-06-01 16:03:33 -0500

    Utilizando formularios para enviar datos(URL), luego obtener esos datos(GET), los sumamos(python) y los mostramos(HTML)

--- Laboratorio7-DjangoTuristico/projects/ProyectoTelusko/calc/views.py
+++ Laboratorio7-DjangoTuristico/projects/ProyectoTelusko/calc/views.py
@@ -6,3 +6,10 @@ def home(request,*args, **kwargs) :
     print(args,kwargs)
     print(request.user)
     return render(request, 'home.html', { 'name':'Diego' })
+
+def add(request) :
+    val1=int(request.GET['num1'])
+    val2=int(request.GET['num2'])
+    reslt=val1+val2
+    return render(request, 'result.html', { 'result':reslt })
+    
