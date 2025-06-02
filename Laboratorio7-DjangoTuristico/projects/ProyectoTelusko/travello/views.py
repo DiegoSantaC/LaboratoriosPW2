43c4cfeb054e17913cdbf0b71606a3877b9c5d14
Diego <dsantacruz@unsa.edu.pe>
2025-06-01 19:45:57 -0500

    Creando un modelo(clase) para mis detinos turisticas y usando los datos de esta clase para hacer dinamica mi pagina principal

--- Laboratorio7-DjangoTuristico/projects/ProyectoTelusko/travello/views.py
+++ Laboratorio7-DjangoTuristico/projects/ProyectoTelusko/travello/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 def index(request,*args, **kwargs) :
-    print(args,kwargs)
     print(request.user)
-    return render(request, 'index.html')
+    dest1 = Destination()
+    dest1.name = 'Perú'
+    dest1.desc = 'La mejor gastronomia'
+    return render(request, 'index.html', { 'dest1' : dest1})
