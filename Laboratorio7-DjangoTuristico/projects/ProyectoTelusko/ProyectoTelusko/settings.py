61d52d5c1d869649d9275e66527645c9bb6bea45
Diego <dsantacruz@unsa.edu.pe>
2025-06-02 16:28:54 -0500

    Creando la base de datos y migrando el modelo destination hacia esta junto a los otros archivos de django(Usando sqlite)

--- Laboratorio7-DjangoTuristico/projects/ProyectoTelusko/ProyectoTelusko/settings.py
+++ Laboratorio7-DjangoTuristico/projects/ProyectoTelusko/ProyectoTelusko/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'travello.apps.TravelloConfig',
 ]
 
 MIDDLEWARE = [
@@ -65,7 +66,7 @@ WSGI_APPLICATION = 'ProyectoTelusko.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'telusko.db',
     }
 }
 
