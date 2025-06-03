9d6b098069a37e4b040814e9d8a712e04816362c
Diego <dsantacruz@unsa.edu.pe>
2025-06-02 22:53:02 -0500

    Agregando formularios para el registro de destinos, agregando un css para los formularios y terminando el laboratorio 7-------------

--- Laboratorio7-DjangoTuristico/projects/ProyectoTelusko/travello/urls.py
+++ Laboratorio7-DjangoTuristico/projects/ProyectoTelusko/travello/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('listar', views.listar, name='listar'),
+    path('listar/eliminar/<int:id>/', views.eliminar, name='eliminar'),
+    path('listar/agregar/', views.agregar, name='agregar'),
 ]
--- Laboratorio7-DjangoTuristico/projects/ProyectoTelusko/travello/views.py
+++ Laboratorio7-DjangoTuristico/projects/ProyectoTelusko/travello/views.py
@@ -1,10 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from .models import Destination
 
 # Create your views here.
 def index(request,*args, **kwargs) :
-    print(request.user)
     dests = Destination.objects.all()
-    print(dests)
 
     return render(request, 'index.html', { 'dests' : dests })
+
+def listar(request):
+    dests = Destination.objects.all()
+
+    return render(request, 'admin.html', { 'dests' : dests })
+
+def agregar(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        desc = request.POST.get('desc')
+        price = request.POST.get('price')
+        offert = 'offert' in request.POST  # Checkbox
+        img = request.FILES.get('img')
+
+        destino = Destination(
+            name=name,
+            desc=desc,
+            price=price,
+            offert=offert,
+            img=img
+        )
+        destino.save()
+        messages.success(request, 'Destino agregado correctamente.')
+        return redirect('listar')
+
+    return render(request, 'agregar.html')
+
+def eliminar(request, id):
+    destino = get_object_or_404(Destination, id=id)
+    destino.delete()
+
+    return redirect('listar')
