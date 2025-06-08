efd0564e02d928bf11e79b29ab2da87d82c1f998
Diego <dsantacruz@unsa.edu.pe>
2025-06-08 16:45:24 -0500

    Implementando la relacion 1 a muchos entre mis tablas Lenguaje y framework con ForeingKey en mi clase hija Framework

--- Laboratorio8-Django3/lab8/Relaciones/admin.py
+++ Laboratorio8-Django3/lab8/Relaciones/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Simple,Lenguaje,Fecha,Nulo,FrameWork
 
 # Register your models here.
+admin.site.register(Simple)
+admin.site.register(Lenguaje)
+admin.site.register(Fecha)
+admin.site.register(Nulo)
+admin.site.register(FrameWork)
--- Laboratorio8-Django3/lab8/Relaciones/models.py
+++ Laboratorio8-Django3/lab8/Relaciones/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Simple(models.Model):
-    texto = models.CharField(max_length='10')
-    numero = models.ImageField(null=True)
+    texto = models.CharField(max_length=10)
+    numero = models.IntegerField(null=True)
     url = models.URLField(default='www.example.com')
 
     def __str__(self):
@@ -16,7 +16,11 @@ class Nulo(models.Model):
     col = models.CharField(max_length=10, blank=True, null=True)
 
 class Lenguaje(models.Model):  # 1 Lenguaje puede tener varios Framework
-    pass
+    name= models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
 
 class FrameWork(models.Model):  # 1 Framework solo puede tener 1 lenguaje
-    pass
+    name= models.CharField(max_length=10)
+    lenguaje = models.ForeignKey(Lenguaje, on_delete=models.CASCADE) 
