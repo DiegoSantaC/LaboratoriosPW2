175c293910407e3b7831d3eec443fe66529012bf
Diego <dsantacruz@unsa.edu.pe>
2025-06-08 16:16:56 -0500

    Creando modelos ya hechos en el video(Simple, Fecha, Nulo, Lenguaje y Framework) para probar el primer tipo de relacion

--- Laboratorio8-Django3/lab8/Relaciones/models.py
+++ Laboratorio8-Django3/lab8/Relaciones/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Simple(models.Model):
+    texto = models.CharField(max_length='10')
+    numero = models.ImageField(null=True)
+    url = models.URLField(default='www.example.com')
+
+    def __str__(self):
+        return self.url
+    
+class Fecha(models.Model):
+    fecha= models.DateTimeField()
+
+class Nulo(models.Model):
+    col = models.CharField(max_length=10, blank=True, null=True)
+
+class Lenguaje(models.Model):  # 1 Lenguaje puede tener varios Framework
+    pass
+
+class FrameWork(models.Model):  # 1 Framework solo puede tener 1 lenguaje
+    pass
