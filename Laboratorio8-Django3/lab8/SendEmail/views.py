28b35da06d87b25f58f3247bebf94c22e21b14a7
Diego <dsantacruz@unsa.edu.pe>
2025-06-08 21:11:18 -0500

    Agregando configuracion en settings para el envio de emails e implementando el views para que funcione correctamente

--- Laboratorio8-Django3/lab8/SendEmail/views.py
+++ Laboratorio8-Django3/lab8/SendEmail/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
+def index(request):
+    send_mail('Hello from PrettyPrinted', 
+    'Hello there. This is an automated message',
+    'diegosantacruzvila@gmail.com',
+    ['dsantacruzv@unsa.edu.pe'],
+    fail_silently=False )
+    
+    return render(request, 'send/index.html')
