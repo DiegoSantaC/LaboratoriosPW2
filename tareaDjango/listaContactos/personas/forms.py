6de98049aa437051501fb49af9e1c176750a094f
Diego <dsantacruz@unsa.edu.pe>
2025-06-28 11:54:49 -0500

    Modificando los campos de mi formulario, usando widget, label e initial como cores

--- tareaDjango/listaContactos/personas/forms.py
+++ tareaDjango/listaContactos/personas/forms.py
@@ -12,7 +12,16 @@ class PersonaForm(forms.ModelForm):
         ]
 
 class RawPersonaForm(forms.Form):
-    nombres = forms.CharField()
+    nombres = forms.CharField(label="Tu Nombre: ", 
+                widget= forms.Textarea(
+                    attrs={
+                        'placeholder': 'Solo tu nombre por favor',
+                        'id': 'NombreID',
+                        'class': 'special',
+                        'cols': 10,
+                    }
+                )
+            )
     apellidos = forms.CharField()
     edad = forms.IntegerField(initial=22)
     donador = forms.BooleanField()
