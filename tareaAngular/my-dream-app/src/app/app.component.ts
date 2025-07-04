54ff257c1d1412939f37d3c451893fd7eff97f99
Diego <dsantacruz@unsa.edu.pe>
2025-07-03 19:16:09 -0500

    Usando modulos ngIf y ngFor en mi html, implementando constructor en mi .ts y agregando la lista de hobbies

--- tareaAngular/my-dream-app/src/app/app.component.ts
+++ tareaAngular/my-dream-app/src/app/app.component.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-dream-app';
-  name = 'Diego Santa Cruz Villa';
-  email = 'dsantacruzv@unsa.edu.pe';
-  webpage = "http://www.unsa.edu.pe";
+  name : string;
+  email;
+  webpage : string;
+  hobbies : string[];
+
+  constructor(){
+    console.log("Constructor trabajando...");
+    this.name = 'Diego Santa Cruz Villa';
+    this.email = 'dsantacruzv@unsa.edu.pe';
+    this.webpage = "http://www.unsa.edu.pe";
+    this.hobbies = ["Videojuegos","Basquet","Programacion","Lectura"];
+  }
+
+  showhobbies() {
+    return true;
+  }
 }
