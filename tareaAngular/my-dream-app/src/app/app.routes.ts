7c865149292e9126657778c0a732b7163370f844
Diego <dsantacruz@unsa.edu.pe>
2025-07-05 18:28:56 -0500

    Generando una aplicacion de peliculas con videos incrustados, usando la etiqueta iframe para esto, toda la implementacion en el componente peliculas y generando su ruta en mi aplicacion principal

--- tareaAngular/my-dream-app/src/app/app.routes.ts
+++ tareaAngular/my-dream-app/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HelloWordComponent } from './hello-word/hello-word.component';
 import { DataComponent } from './data/data.component';
+import { PeliculasComponent } from './peliculas/peliculas.component';
 
 export const routes: Routes = [
     {path: 'about', component:AboutComponent},
     {path: 'hello', component:HelloWordComponent},
     {path: 'data', component:DataComponent},
+    {path: 'peliculas', component:PeliculasComponent},
 ];
---
+++ tareaAngular/my-dream-app/src/app/peliculas/peliculas.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-peliculas',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './peliculas.component.html',
+  styleUrl: './peliculas.component.css'
+})
+export class PeliculasComponent {
+  title= "Peliculas";
+  videoList: { name: string, slug: string, url: string }[] = [];
+  names: string[];
+  urls: string[];
+
+  constructor(private sanitizer: DomSanitizer){
+    this.names=["Avengers:InfinityWar","Thor:Ragnarok","Avengers:EndGame","Spider-man: No way home"];
+    this.urls=[
+      "https://www.youtube.com/watch?v=823iAZOEKt8",
+      "https://www.youtube.com/watch?v=ue80QwXMRHg",
+      "https://www.youtube.com/watch?v=TcMBFSGVi1c",
+      "https://www.youtube.com/watch?v=JfVOs4VSpmA",
+    ]
+
+    this.generateVideoList();
+  }
+  
+  generateVideoList(){
+    this.videoList = [];
+    for(let i=0;i<this.names.length;i++)
+      this.videoList.push({
+        name: this.names[i],
+        slug: this.createSlug(this.names[i]),
+        url: this.urls[i],
+      });
+  }
+
+  createSlug(text: string): string{
+    return text
+    .toLowerCase()
+    .replace(/:/g, '')
+    .replace(/\s+/g, '-')
+    .replace(/[^\w\-]+/g, '');
+  }
+
+  getYouTubeId(url: string): string {
+    try {
+      const urlObj = new URL(url);
+      return urlObj.searchParams.get("v") || '';
+    } catch {
+      return '';
+    }
+  }
+
+  getEmbedUrl(videoUrl: string): SafeResourceUrl {
+    const id = this.getYouTubeId(videoUrl);
+    return this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${id}`);
+  }
+}
