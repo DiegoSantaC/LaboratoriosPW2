5a992d95048b2790b16fa7d36afb693291fc8765
Diego <dsantacruz@unsa.edu.pe>
2025-07-10 22:38:45 -0500

    --------------Commit para terminar la tarea de djangular, cambios minimos

--- tareaDjangular/djangocrud/api/serializer.py
+++ tareaDjangular/djangocrud/api/serializer.py
@@ -5,4 +5,4 @@ from .models import Movie
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ('id','nombre','url','logo')
+        fields = ('nombre','url','logo')
