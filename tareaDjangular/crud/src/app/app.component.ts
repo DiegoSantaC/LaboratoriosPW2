e341ea5a320dc0f181e91da4329006818abd38a1
Diego <dsantacruz@unsa.edu.pe>
2025-07-10 20:07:38 -0500

    Arreglando el typescript para la version moderna y agregando provideHttpClient en mi config para poder obtener la data correctamente

--- tareaDjangular/crud/src/app/app.component.ts
+++ tareaDjangular/crud/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ApiService } from './api.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -21,7 +23,7 @@ export class AppComponent {
     this.api.getAllMovies().subscribe (
       data => {
         console.log(data);
-        //this.movies = data;  //data.results;
+        this.movies = data;  //data.results;
       },
       error => {
         console.log(error);
